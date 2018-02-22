0bb50adcfc8079fbd74db656ec679d86f3e9c0e6
Albert Balbastre-Morte <bulbastre@gmail.com>
2018-02-22 21:15:05 +0100

    INITIAL COMMIT

---
+++ src/action/LoginAction.java
@@ -0,0 +1,30 @@
+package action;
+
+public class LoginAction{
+
+	private String username2;
+	private String password;
+	 
+	public String getUsername() {
+		System.out.println(username2);
+		System.out.println(password);
+		return username2;
+	}
+
+	public void setUsername2(String username) {
+		this.username2 = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String execute() {
+		
+		
+		if (this.username2.equalsIgnoreCase("admin"))
+			return "SUCCESS";
+		else
+			return "ERROR";
+
+	}
+}
