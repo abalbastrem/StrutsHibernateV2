376c251c35dabab4632f567260476c7859c239bd
Albert Balbastre-Morte <bulbastre@gmail.com>
2018-02-26 20:06:50 +0100

    duplicated struts.xml??

---
+++ WebContent/User/pages/jugadores.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Jugadores</title>
+</head>
+<body>
+
+	<ol>
+	<s:iterator value="jugadores">
+		<li><s:property /></li>
+	</s:iterator>
+	</ol>
+
+</body>
+</html>
---
+++ src/action/Populate.java
@@ -0,0 +1,133 @@
+package action;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+
+import action.HibernateUtil;
+
+public class Populate {
+
+	// Populates database
+	public static void main(String[] args) {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		JugadorAction j1 = new JugadorAction();
+		j1.setNombre("Sex Fabregas");
+		j1.setnGoles(69);
+		
+		JugadorAction j2 = new JugadorAction();
+		j2.setNombre("Cristiano Chulazo");
+		j2.setnGoles(11);
+		
+		JugadorAction j3 = new JugadorAction();
+		j3.setNombre("Messi Camxot");
+		j3.setnGoles(99999);
+		
+		JugadorAction j4 = new JugadorAction();
+		j4.setNombre("Andrés Inieste Gusiluz");
+		j4.setnGoles(57);
+		
+		JugadorAction j5 = new JugadorAction();
+		j5.setNombre("Piqué President");
+		j5.setnGoles(58);
+		
+		JugadorAction j6 = new JugadorAction();
+		j6.setNombre("Benzemá Finsdemà");
+		j6.setnGoles(24);
+		
+		JugadorAction j7 = new JugadorAction();
+		j7.setNombre("Oliver Atom");
+		j7.setnGoles(987654321);
+		
+		JugadorAction j8 = new JugadorAction();
+		j8.setNombre("Oliver no, el otro");
+		j8.setnGoles(987654321);
+		
+		ArrayList<JugadorAction> jugadoresE1 = new ArrayList<>();
+		jugadoresE1.add(j1);
+		jugadoresE1.add(j3);
+		jugadoresE1.add(j4);
+		jugadoresE1.add(j5);
+		
+		ArrayList<JugadorAction> jugadoresE2 = new ArrayList<>();
+		jugadoresE2.add(j2);
+		jugadoresE2.add(j6);
+		
+		ArrayList<JugadorAction> jugadoresE3 = new ArrayList<>();
+		jugadoresE3.add(j1);
+		jugadoresE3.add(j2);
+		
+		ArrayList<JugadorAction> jugadoresE4 = new ArrayList<>();
+		jugadoresE4.add(j7);
+		jugadoresE4.add(j8);
+		
+		EquipoAction e1 = new EquipoAction();
+		e1.setNombre("Barsa");
+		e1.setJugadores(jugadoresE1);
+		
+		EquipoAction e2 = new EquipoAction();
+		e2.setNombre("Hala Madrí");
+		e2.setJugadores(jugadoresE2);
+		
+		EquipoAction e3 = new EquipoAction();
+		e3.setNombre("Poble Sec");
+		e3.setJugadores(jugadoresE3);
+		
+		EquipoAction e4 = new EquipoAction();
+		e4.setNombre("Gavilanes");
+		e4.setJugadores(jugadoresE4);
+		
+		ArrayList<EquipoAction> equiposL1 = new ArrayList<>();
+		equiposL1.add(e1);
+		equiposL1.add(e3);
+		
+		ArrayList<EquipoAction> equiposL2 = new ArrayList<>();
+		equiposL2.add(e1);
+		equiposL2.add(e2);
+		
+		ArrayList<EquipoAction> equiposL3 = new ArrayList<>();
+		equiposL3.add(e4);
+		equiposL3.add(e3);
+		
+		LigaAction l1 = new LigaAction();
+		l1.setNombre("Eurocopa");
+		l1.setEquipos(equiposL1);
+		
+		LigaAction l2 = new LigaAction();
+		l2.setNombre("Copa del Rey");
+		l2.setEquipos(equiposL2);
+		
+		LigaAction l3 = new LigaAction();
+		l3.setNombre("Mundial");
+		l3.setEquipos(equiposL3);
+		
+		session.save(l1);
+		session.save(l2);
+		session.save(l3);
+		
+		session.save(e1);
+		session.save(e2);
+		session.save(e3);
+		session.save(e4);
+		
+		session.save(j1);
+		session.save(j2);
+		session.save(j3);
+		session.save(j4);
+		session.save(j5);
+		session.save(j6);
+		session.save(j7);
+		session.save(j8);
+		
+		session.getTransaction().commit();
+		
+		session.close();
+		
+		System.out.println("DATABASE SHOULD BE POPULATED CORRECTLY.");
+
+	}
+
+}
